919b5b8dbb02257b4a0ef0016f25c60d07bba314
SiphokaziFikeni <dumisile.fikeni@gmail.com>
2018-01-25 13:49:37 +0200

    Added base Splash Activity

---
+++ app/src/main/java/com/example/sfikeni/remindmi/activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.sfikeni.remindmi.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sfikeni.remindmi.MainActivity;
+import com.example.sfikeni.remindmi.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 3000);
+    }
+
+    private void startMainActivity() {
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(mainActivityIntent);
+        finish();
+    }
+}
