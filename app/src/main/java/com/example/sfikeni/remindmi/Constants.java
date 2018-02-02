78a8bca513b82d89d4b102ff52757c5af2c37061
SiphokaziFikeni <dumisile.fikeni@gmail.com>
2018-02-02 10:36:49 +0200

    Added basic Realm Configuration

---
+++ app/src/main/java/com/example/sfikeni/remindmi/Constants.java
@@ -0,0 +1,9 @@
+package com.example.sfikeni.remindmi;
+
+/**
+ * Created by SFikeni on 2018/02/02.
+ */
+
+public class Constants {
+    public static String REALM_DATABASE_NAME = "remindmi_realm_database";
+}
